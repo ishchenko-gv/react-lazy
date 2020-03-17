0a75fe075aef0e6ad1ca441ff59f62006835e301
Georgii Ishchenko <gvishenko@domclick.ru>
2020-03-18 01:23:24 +0300

    dev

--- src/parts/gallery/index.js
+++ src/parts/gallery/index.js
@@ -1,17 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { searchMovie } from '../../services/api-request';
 
 export default function Gallery() {
+  const [movies, setMovies] = useState([]);
+
   useEffect(() => {
     const fetchMovies = async () => {
-      console.log(await searchMovie('batman'))
+      setMovies(await searchMovie('batman'))
     };
 
     fetchMovies();
-  });
+  }, []);
 
   return (
-    <div>Gallery</div>
+    <div>
+      <h2>Gallery</h2>
+      <input type='text' onChange={async e => setMovies(await searchMovie(e.target.value))} />
+      {movies && movies.map(movie => (
+        <div key={movie.imdbID}>
+          <div>{movie.Title}</div>
+          <div>{movie.Year}</div>
+          <img src={movie.Poster} />
+        </div>
+      ))}
+    </div>
   );
 }
